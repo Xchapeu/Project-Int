16f9616404ca54b0ad919ede58593322453e557f
xchapeu <xchapeu@gmail.com>
2023-04-19 07:30:54 -0300

    Add first name funcionality to main content page

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -25,16 +25,22 @@ export const MainContent = () => {
     const [racasArray, setRacasArray] = useState([])
     const [raca, setRaca] = useState(racasArray[0]);
     const [petGender, setPetGender] = useState("macho");
-    const [v8, setV8] = useState(Date());
-    const [v10, setV10] = useState(Date());
-    const [antirabica, setAntirabica] = useState(Date())
-    const [gripeCanina, setGripeCanina] = useState(Date())
-    const [leishmaniose, setLeishmaniose] = useState(Date())
+    const [vermifugo, setVermifugo] = useState("");
+    const [isChipped, setIsChipped] = useState(false);
+    const [isSterilized, setIsSterilized] = useState(false);
+    const [v8, setV8] = useState("");
+    const [v10, setV10] = useState("");
+    const [antirabica, setAntirabica] = useState("");
+    const [gripeCanina, setGripeCanina] = useState("");
+    const [giardia, setGiardia] = useState("");
+    const [leishmaniose, setLeishmaniose] = useState("");
 
-    const [tutor, setTutor] = useState("");  
+    const [tutor, setTutor] = useState("");
 
     const handlePetFormSubmit = (e) => {
         e.preventDefault();
+
+        console.log(petNome, petIdade, raca, petGender, vermifugo, v8, v10, antirabica, gripeCanina, giardia, leishmaniose);
 
         alert("Pet cadastrado com sucesso!");
     }
@@ -76,12 +82,26 @@ export const MainContent = () => {
                     <form onSubmit={handlePetFormSubmit}>
                         <fieldset className="field">
                             <label htmlFor="petNome">Nome do pet</label>
-                            <input type="text" id="petNome" name="petNome" />
+                            <input 
+                                type="text" 
+                                id="petNome" 
+                                name="petNome"
+                                value={petNome}
+                                onChange={e => setPetNome(e.target.value)}
+                                required
+                            />
                         </fieldset>
                         
                         <fieldset className="field">
                             <label htmlFor="petIdade">Idade do pet</label>
-                            <input type="number" id="petIdade" name="petIdade" />
+                            <input 
+                                type="number" 
+                                id="petIdade" 
+                                name="petIdade"
+                                value={petIdade}
+                                onChange={e => setPetIdade(e.target.value)}
+                                required
+                            />
                         </fieldset>
                         
                         <fieldset className="field">
@@ -94,7 +114,7 @@ export const MainContent = () => {
                         
                         <fieldset className="petGender">
                             <label htmlFor="petGender">Sexo:
-                                <select name="petGender" id="petGender" value={petGender} onChange={e => setPetGender(e.target.value)}>
+                                <select name="petGender" id="petGender" value={petGender} onChange={e => setPetGender(e.target.value)} required>
                                     <option value="macho">Macho</option>
                                     <option value="femea">Fêmea</option>
                                 </select>
@@ -104,36 +124,101 @@ export const MainContent = () => {
 
                         <fieldset className="field">
                             <label htmlFor="vermifugo">Última vermifugação:</label>
-                            <input type="date" id="vermifugo" name="vermifugo" />
+                            <input 
+                                type="date" 
+                                id="vermifugo" 
+                                name="vermifugo"
+                                value={vermifugo}
+                                onChange={e => setVermifugo(e.target.value)}
+                            />
                         </fieldset>
 
                         <fieldset className="hasChip">
                             <label htmlFor="petChip">Possui chip: </label>
-                            <input type="checkbox" id="petChip" name="petChip" />
+                            <input 
+                                type="checkbox" 
+                                id="petChip" 
+                                name="petChip" 
+                            />
                         </fieldset>
 
                         <fieldset>
                             <label htmlFor="petCastrado">Castração: </label>
-                            <input type="checkbox" id="petCastrado" name="petCastrado" />
+                            <input 
+                                type="checkbox" 
+                                id="petCastrado" 
+                                name="petCastrado" 
+                            />
                         </fieldset>
 
                         <div className="vaccines">
                             <label className="title">Vacinas:</label>
-                            {/* <fieldset className="field">
+                            <fieldset className="field">
                                 <label htmlFor="v8">V8 - ultima vacinação: </label>
-                                <input type="date" id="v8" name="v8" />
+                                <input 
+                                    type="date" 
+                                    id="v8" 
+                                    name="v8"
+                                    value={v8}
+                                    onChange={e => setV8(e.target.value)} 
+                                />
                             </fieldset>
 
                             <fieldset className="field">
                                 <label htmlFor="v10">V10 - ultima vacinação: </label>
-                                <input type="date" id="v10" name="v10" />
+                                <input 
+                                    type="date" 
+                                    id="v10" 
+                                    name="v10"
+                                    value={v10}
+                                    onChange={e => setV10(e.target.value)}
+                                />
                             </fieldset>
 
                             <fieldset className="field">
                                 <label htmlFor="antirabica">Antirábica - ultima vacinação: </label>
-                                <input type="date" id="antirabica" name="antirabica" />
-                            </fieldset> */}
-                            {
+                                <input 
+                                    type="date" 
+                                    id="antirabica" 
+                                    name="antirabica"
+                                    value={antirabica}
+                                    onChange={e => setAntirabica(e.target.value)} 
+                                />
+                            </fieldset>
+
+                            <fieldset className="field">
+                                <label htmlFor="gripeCanina">Gripe Canina - ultima vacinação: </label>
+                                <input 
+                                    type="date" 
+                                    id="gripeCanina" 
+                                    name="gripeCanina"
+                                    value={gripeCanina}
+                                    onChange={e => setGripeCanina(e.target.value)}
+                                />
+                            </fieldset>
+
+                            <fieldset className="field">
+                                <label htmlFor="giardia">Giardia - ultima vacinação: </label>
+                                <input 
+                                    type="date" 
+                                    id="giardia" 
+                                    name="giardia"
+                                    value={giardia}
+                                    onChange={e => setGiardia(e.target.value)} 
+                                />
+                            </fieldset>
+
+                            <fieldset className="field">
+                                <label htmlFor="leishmaniose">Leishmaniose - ultima vacinação: </label>
+                                <input 
+                                    type="date" 
+                                    id="leishmaniose" 
+                                    name="leishmaniose"
+                                    value={leishmaniose}
+                                    onChange={e => setLeishmaniose(e.target.value)} 
+                                />
+                            </fieldset>
+                            {/* {
                                 vaccines.map(vaccine => {
 
                                     return (
@@ -147,7 +232,7 @@ export const MainContent = () => {
                                         </fieldset>
                                     )
                                 })
-                            }
+                            } */}
 
                             <input type="submit" value="Cadastrar pet" className="btn-pet-submit" />
                         </div>
