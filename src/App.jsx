c15ea4efca4914931e0cc1986a335cfc41882714
xchapeu <xchapeu@gmail.com>
2023-06-01 10:33:36 -0300

    Add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { AppRoutes } from "./AppRoutes";
+import { Analytics } from '@vercel/analytics/react';
 import "./global.css";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <AppRoutes />
+      <Analytics />
     </div>
   )
 }
