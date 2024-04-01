26c2b10c9917637f75ff5cc58386ff0a2f4905b3
Xchapeu <xchapeu@gmail.com>
2024-04-01 20:16:13 -0300

    Implementação de nivel e quantidades de pontos para proximo nivel

---
+++ levels.js
@@ -0,0 +1,42 @@
+export const levels = [
+    {
+        level: 1,
+        minPoints: 0
+    },
+    {
+        level: 2,
+        minPoints: 100
+    },
+    {
+        level: 3,
+        minPoints: 250
+    },
+    {
+        level: 4,
+        minPoints: 500
+    },
+    {
+        level: 5,
+        minPoints: 1000
+    },
+    {
+        level: 6,
+        minPoints: 1500
+    },
+    {
+        level: 7,
+        minPoints: 2000
+    },
+    {
+        level: 8,
+        minPoints: 3000
+    },
+    {
+        level: 9,
+        minPoints: 3750
+    },
+    {
+        level: 10,
+        minPoints: 4900
+    }
+]
--- src/pages/TrophiesPage/index.jsx
+++ src/pages/TrophiesPage/index.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from "../../contexts/auth";
 import { useContext, useState, useEffect } from "react";
 import { Footer } from '../../components/Footer';
 import { achievements } from '../../../achievements';
+import { levels } from '../../../levels';
 import "./styles.css";
 import { TopButton } from '../../components/TopButton';
 
@@ -13,6 +14,9 @@ export const TrophiesPage = () => {
     const [items, setItems] = useState(achievements);
     const [uncompleted, setUncompleted] = useState(items);
     const [completed, setCompleted] = useState([]);
+    const [totalAmount, setTotalAmount] = useState(0);
+    const [currentLevel, setCurrentLevel] = useState(0);
+    const [pointsNeededForNextLevel, setPointsNeededForNextLevel] = useState(0)
 
     const handleCheckboxChange = (itemId) => {
         setItems(prevItems => {
@@ -27,6 +31,25 @@ export const TrophiesPage = () => {
         });
     };
 
+    const getCurrentLevel = (totalCompletedAmount) => {
+        let currentLevel = 1;
+    
+        for (const levelData of levels) {
+            if (totalCompletedAmount >= levelData.minPoints) {
+                currentLevel = levelData.level;
+            } else {
+                setPointsNeededForNextLevel(levelData.minPoints - totalCompletedAmount);
+                break; 
+            }
+        }
+    
+        if (currentLevel === levels[levels.length - 1].level) {
+            setPointsNeededForNextLevel(0);
+        }
+    
+        return currentLevel;
+    };
+
     const handleLogout = () => {
         logout();
     }
@@ -35,8 +58,13 @@ export const TrophiesPage = () => {
         const uncompletedItems = items.filter(item => !item.completed);
         const completedItems = items.filter(item => item.completed);
     
+        const totalCompletedAmount = completedItems.reduce((total, item) => total + parseInt(item.xpAmount), 0);
+        console.log(totalCompletedAmount);
+
         setUncompleted(uncompletedItems);
         setCompleted(completedItems);
+        setTotalAmount(totalCompletedAmount);
+        setCurrentLevel(getCurrentLevel(totalCompletedAmount));
     }, [items]);
 
     return (
@@ -55,6 +83,7 @@ export const TrophiesPage = () => {
                             <h3 className='achievements-list-title'>Tarefas não concluídas</h3>
                             <ul className='achievements-list uncompleted-list'>
                                 {
+                                    uncompleted.length > 0 ?
                                     uncompleted.map(item => {
                                         let achievement = `achievement${item.id}`;
                                         return (
@@ -67,11 +96,11 @@ export const TrophiesPage = () => {
                                                         data-amount={item.xpAmount}
                                                         onChange={() => handleCheckboxChange(item.id)}
                                                     />
-                                                    <label htmlFor={achievement}>{item.title} - <span className='xp-amount'>{item.xpAmount} exp</span></label>
+                                                    <label htmlFor={achievement}>{item.title} - <span className='xp-amount'>{item.xpAmount} pontos</span></label>
                                                 </div>
                                             </li>
                                         )
-                                    })
+                                    }) : (<h3 className='empty-list-message'>Parabéns!!<br/>Você desbloqueou todas as conquistas!</h3>)
                                 }
                             </ul>
                         </div>
@@ -79,6 +108,7 @@ export const TrophiesPage = () => {
                             <h3 className='achievements-list-title'>Tarefas concluídas</h3>
                             <ul className='achievements-list completed-list'>
                                 {
+                                    completed.length > 0 ?
                                     completed.map(item => {
                                         let achievement = `achievement${item.id}`;
                                         return (
@@ -92,18 +122,24 @@ export const TrophiesPage = () => {
                                                         onChange={() => handleCheckboxChange(item.id)} 
                                                         checked 
                                                     />
-                                                    <label htmlFor={achievement}>{item.title} - <span className='xp-amount'>{item.xpAmount} exp</span></label>
+                                                    <label htmlFor={achievement}>{item.title} - <span className='xp-amount'>{item.xpAmount} pontos</span></label>
                                                 </div>
                                             </li>
                                         )
-                                    })
+                                    }) : (<h3 className='empty-list-message'>Não há conquistas completadas.<br/> Bora começar!?</h3>)
                                 }
                             </ul>
                         </div>
                     </div>
 
                     <div className='right-container'>
-
+                        <div className="achievements-list-container">
+                            <h3 className='achievements-list-title'>
+                                Total de pontos: {totalAmount} <br/> 
+                                Nivel: {currentLevel}<br/>
+                                Pontos prox nivel: {pointsNeededForNextLevel} 
+                            </h3>
+                        </div>
                     </div>
                 </div>
             </div>
